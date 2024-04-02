83427c3a8171e177fe3a903cd65c4ffe2cf0603c
William Byers <billybyers1998@gmail.com>
2024-04-02 21:41:11 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+const astrology = (name, age, birthMonth) => {
+  if (birthMonth === "january") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "february") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "march") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "april") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "may") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "june") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "july") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "august") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "september") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "october") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "november") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else if (birthMonth === "december") {
+    console.log(
+      `${name}, you are ${age} years old and you were born in ${birthMonth}`
+    );
+  } else {
+    console.log("invalid month");
+  }
+};
+
+astrology("John", 25, "march");
